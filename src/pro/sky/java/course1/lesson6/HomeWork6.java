85be3273b3761606dbab7c271dca07e0e387b0a9
Huawei <meelo.k@mail.ru>
2021-09-16 14:20:11 +0300

    Mistakes in lesson6 ("Methods") is fixed

--- src/pro/sky/java/course1/lesson6/HomeWork6.java
+++ src/pro/sky/java/course1/lesson6/HomeWork6.java
@@ -1,13 +1,11 @@
 package pro.sky.java.course1.lesson6;
 
-import java.util.Arrays;
-
 public class HomeWork6 {
     public static void main(String[] args) {
-
+        System.out.println(calculateAverageSalary(generateRandomArray()));
     }
 
-    public static void task1(int year) {
+    public static void checkIsLeapYear(int year) {
         if (!(year % 100 == 0 && year % 400 != 0 || year % 4 != 0)) {
             System.out.println(year + " - високосный год");
         } else {
@@ -15,7 +13,7 @@ public class HomeWork6 {
         }
     }
 
-    public static void task2(int clientOS, int clientDeviceYear) {
+    public static void printVersionAndSystemOS(int clientOS, int clientDeviceYear) {
         if (clientOS == 0) {
             if (clientDeviceYear < 2019) {
                 System.out.println("Установите lite-версию приложения для IOS по ссылке.");
@@ -31,7 +29,7 @@ public class HomeWork6 {
         }
     }
 
-    public static void task3(int deliveryDistance) {
+    public static String calculateDeliveryDistance(int deliveryDistance) {
         int day = 1;
         if (deliveryDistance >= 20) {
             day++;
@@ -39,37 +37,34 @@ public class HomeWork6 {
         if (deliveryDistance >= 60) {
             day++;
         }
-        System.out.println("Количество дней для доставки : " + day);
+        return "Количество дней для доставки : " + day;
     }
 
-    public static void task4(String str) {
+    public static void checkDoubles(String str) {
         char[] chars = str.toCharArray();
         for (int i = 1; i < chars.length; i++) {
             if (chars[i] == chars[i - 1]) {
                 System.out.println("Дубль найден - " + chars[i]);
-                break;
+                return;
             }
-            if (i == chars.length - 1)
-                System.out.println("Дубль не найден.");
         }
+        System.out.println("Дубль не найден.");
     }
 
-    public static void task5(int[] arrayToReverse) {
+    public static void reverse(int[] arrayToReverse) {
         for (int i = 0, j = arrayToReverse.length - 1; i < j; i++, j--) {
             int temp = arrayToReverse[i];
             arrayToReverse[i] = arrayToReverse[j];
             arrayToReverse[j] = temp;
         }
-        System.out.println(Arrays.toString(arrayToReverse));
     }
 
-    public static void task6(int[] ints) {
-        ints = generateRandomArray();
+    public static String calculateAverageSalary(int[] ints) {
         double sum = 0;
         for (int i : ints) {
             sum += i;
         }
-        System.out.println("Средняя сумма трат за месяц составила " + sum / ints.length + " рублей.");
+        return "Средняя сумма трат за месяц составила " + sum / ints.length + " рублей.";
     }
 
     public static int[] generateRandomArray() {
